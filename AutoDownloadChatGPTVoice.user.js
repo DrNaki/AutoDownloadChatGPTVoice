07be59ad16efac29823af0ed07e34ad552199a90
DrNaki <orrrrrrrrrrt@gmail.com>
2024-05-16 15:02:49 +0900

    Update 1.1: update metadata, file can be downloaded with filename

--- AutoDownloadChatGPTVoice.user.js
+++ AutoDownloadChatGPTVoice.user.js
@@ -1,14 +1,19 @@
 // ==UserScript==
 // @name         Auto Download ChatGPT's voice
-// @namespace    http://tampermonkey.net/
-// @version      1.0
+// @namespace    https://github.com/DrNaki
+// @version      1.1
 // @description  Automatically downloads the audio file when playing chatgpt's read aloud. 当播放chatgpt的朗读时，自动将声音文件下载下来
 // @author       Naki
+// @copyright    2024, Naki (https://github.com/DrNaki)
+// @license      MIT
+// @homepageURL  https://github.com/DrNaki/AutoDownloadChatGPTVoice
+// @supportURL   https://github.com/DrNaki/AutoDownloadChatGPTVoice/issues
 // @match        *://chatgpt.com/*
 // @grant        none
 // @run-at       document-start
+// @downloadURL  https://update.greasyfork.org/scripts/495120/Auto%20Download%20ChatGPT%27s%20voice.user.js
+// @updateURL    https://update.greasyfork.org/scripts/495120/Auto%20Download%20ChatGPT%27s%20voice.user.js
 // ==/UserScript==
-
 
 
 (function() {
@@ -23,6 +28,18 @@
         const url = args[0];
         if (typeof url === 'string' && url.includes('backend-api/synthesize')) {
             console.log('Intercepted fetch:', url);
+
+            // 从URL中提取message_id参数
+            const urlParams = new URL(url).searchParams;
+            const messageId = urlParams.get('message_id');
+
+            // 如果message_id存在，提取其第一个'-'前的内容
+            let fileName = 'response.aac'; // 默认文件名
+            if (messageId) {
+                const prefix = messageId.split('-')[0];
+                fileName = `${prefix}.aac`;
+            }
+
             return originalFetch(...args)
                 .then(response => {
                     // 克隆响应，因为响应只能被读取一次
@@ -35,7 +52,7 @@
 
                         const a = document.createElement('a');
                         a.href = objectURL;
-                        a.download = 'response.aac'; // 设置文件名
+                        a.download = fileName; // 使用提取的文件名
                         document.body.appendChild(a);
                         a.click(); // 模拟点击以下载文件
                         document.body.removeChild(a); // 移除链接元素
